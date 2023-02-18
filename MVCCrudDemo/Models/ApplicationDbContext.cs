79c0bce5ed5bbed00a4d7ab7fedec9549442ca7b
Richard Chalk <barmybrit@gmail.com>
2023-02-18 08:47:40 +0100

    Add project files.

---
+++ MVCCrudDemo/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCCrudDemo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ MVCCrudDemo/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCCrudDemo.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account number")]
+        public string AccountNumber { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Account owner")]
+        public string AccountOwner { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank name")]
+        public string BankName { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
